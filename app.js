802ef3e98fd4b451f8fd1900f124baa07a43e925
İbrahim Ethem Sancar <ibrahimethemsancar@gmail.com>
2022-10-14 15:58:18 +0300

    add static files

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 
 const app = express();
-
 const port = 5000;
+//Template Engine
+app.set("view engine", "ejs");
 
+//Middlewares
+app.use(express.static("public"));
+
+//Routes
 app.get('/', (req, res) =>{
-    res.send('İndex sayfası')
+    res.status(200).render("index", {
+        page_name : "index"
+    });
+});
+app.get('/about', (req, res) =>{
+    res.status(200).render('about', {
+        page_name : "about"
+    });
 })
 
 app.listen(port, () => {
-    console.log(`Serves is listening on port ${port}`);
+    console.log(`Server is listening on port ${port}`);
 })
