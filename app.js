bf0d7742c2dd2912f91e6c1d255eee4ad6f13e4b
İbrahim Ethem Sancar <ibrahimethemsancar@gmail.com>
2022-10-14 18:53:32 +0300

    edit routes and controller folders

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pageRoute = require('./routes/pageRoute');
 
 const app = express();
 const port = 5000;
@@ -9,17 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 //Routes
-app.get('/', (req, res) =>{
-    res.status(200).render("index", {
-        page_name : "index"
-    });
-});
-app.get('/about', (req, res) =>{
-    res.status(200).render('about', {
-        page_name : "about"
-    });
-})
-
+app.use('/',pageRoute);
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 })
