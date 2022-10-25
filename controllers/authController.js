928dea44b1458ed5e3f655a461aa97a421a3f607
İbrahim Ethem Sancar <ibrahimethemsancar@gmail.com>
2022-10-25 16:18:12 +0300

    user roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,10 +6,11 @@ exports.createUser =  async (req, res) => {
   let password = req.body.password;
   let encryptedPassword = await bcrypt.hash(password,10);
   let email = req.body.email;
+  let role = (req.body.role).toLowerCase();
 
   function create() {
     let query = `INSERT INTO online_course_db.users
-        (name, email, password) values ('${name}', '${email}', '${encryptedPassword}')`;
+        (name, email, password, role) values ('${name}', '${email}', '${encryptedPassword}', '${role}')`;
     return new Promise((resolve, reject) => {
       connection.query(query, (err, result) => {
         if (err) {
@@ -24,10 +25,7 @@ exports.createUser =  async (req, res) => {
     const category = await create();
     console.log(category);
     
-      res.status(201).json({
-          status : 'success',
-          message : "The new user is created." 
-        })
+      res.status(201).redirect('/login')
   }
   else{
     res.status(400).json({
